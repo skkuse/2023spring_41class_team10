8ce9ef7fa0d61651c4eee97164a17e84ea89b11d
SeoJeongYeop <sjyswe99@gmail.com>
2023-05-07 22:48:21 +0900

    feat: 기본 라우팅 구조 추가 - 공통 헤더 추가 - react-icons 라이브러리 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import LoginView from './routes/LoginView';
+import NoticeView from './routes/NoticeView';
+
+import RootLayout from './routes/RootLayout';
+import App from './App';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+      {
+        path: '/login',
+        element: <LoginView />
+      },
+      {
+        path: '/notice',
+        element: <NoticeView />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
