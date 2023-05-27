4349b0ccb967c5069c4f3d95677f04692dd66683
SeoJeongYeop <sjyswe99@gmail.com>
2023-05-28 00:06:54 +0900

    feat: Lectures, Users 관련 Admin 설정 추가 - problems Admin 조회에서 order 관련 오류 수정

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+  search_fields = ["id", "name", "github_username", "email"]
+  list_display = ("id", "name", "github_username", "email", "created_at", "last_login", "profile_image_url")
+
+  @admin.display(description='lecture', ordering='lecture__title')
+  def get_lecture(self, obj):
+    return obj.lecture.title 
+  @admin.display(description='field', ordering='field__field')
+  def get_field(self, obj):
+    return obj.field.field 
+
+admin.site.register(User, UserAdmin)
