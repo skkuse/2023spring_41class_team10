c2ea8df55fb232604972f43c5182ad1a44f2e29c
SeoJeongYeop <sjyswe99@gmail.com>
2023-05-31 13:37:15 +0900

    fix: index.jsx merge conflict

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,11 +1,8 @@
-export { default as Home } from './HomePage'
-export { default as Auth } from './AuthPage'
-export { default as Profile } from './ProfilePage'
-export { default as Problem } from './ProblemPage'
-<<<<<<< HEAD
-export { default as Notice} from './NoticePage'
-export { default as Questions} from './QuestionsPage'
-=======
-export { default as Review } from './ReviewPage'
-export { default as Produce } from './ProducePage'
->>>>>>> 355f49e5f4a4378a5dbcbde1c9683ddeee1e3fd7
+export { default as Home } from './HomePage';
+export { default as Auth } from './AuthPage';
+export { default as Profile } from './ProfilePage';
+export { default as Problem } from './ProblemPage';
+export { default as Notice } from './NoticePage';
+export { default as Questions } from './QuestionsPage';
+export { default as Review } from './ReviewPage';
+export { default as Produce } from './ProducePage';
