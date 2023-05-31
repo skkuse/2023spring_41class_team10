d01d984851c5268da7da65e05a08f0efe5df973d
ybuser <ybuser@naver.com>
2023-06-01 01:40:17 +0900

    style: Navbar 관련 파일 styled component 이용하도록 코드 포매팅

--- frontend/src/components/NavBarBeforeLogin.jsx
+++ frontend/src/components/NavBarBeforeLogin.jsx
@@ -1,17 +1,89 @@
 import React from 'react';
-import styles from './NavBar.module.css'
+import styled from 'styled-components';
+
+const NavigationLeft = styled.div`
+    background-color: #ffffff;
+    height: 64px;
+    left: 318px;
+    position: absolute;
+    top: 0;
+    width: 1440px;
+`;
+
+const FirstButton = styled.button`
+    color: #000000;
+    font-family: "Roboto", Helvetica;
+    font-size: 18px;
+    font-weight: 400;
+    height: 40px;
+    right: 11%;
+    position: absolute;
+    top: 12px;
+    width: 10%;
+    background-color: #ffffff;
+`;
+
+const SecondButton = styled.button`
+    height: 40px;
+    right: 0.7%;
+    position: absolute;
+    top: 12px;
+    width: 10%;
+    background-color: rgb(0,0,0);
+    color: rgb(255,255,255);
+`;
+
+const LinkDiv = styled.div`
+    color: #000000;
+    font-family: "Roboto", Helvetica;
+    font-size: 18px;
+    font-weight: 400;
+    height: 22px;
+    left: 0;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 0;
+`;
+
+const Links2 = styled.div`
+    height: 22px;
+    left: 156px;
+    position: absolute;
+    top: 21px;
+    width: 165px;
+`;
+
+const NavFont = styled.a`
+    font-family: "Roboto", Helvetica; font-size: 18px; font-weight: 400; color: rgb(0, 0, 0); line-height: normal;
+    height: 22px;
+    left: 0;
+    letter-spacing: 0;
+    position: absolute;
+    top: 0;
+`;
+
+const LogoImage = styled.img`
+    height: 58px;
+    left: 20px;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    width: 101px;
+    cursor: pointer;
+`;
 
 function NavBarBeforeLogin(props) {
     return (
-        <div className={styles.navigationleft}>
-            <button className={styles.secondBtn}>Sign Up</button>
-            <button className={styles.firstBtn}>Login</button>
-            <div className={styles.links2}>
-              <div className={styles.link}><a href="#!" className={styles.nav_font}>Home</a></div>
-              <div className={styles.textwrapper}><a href="#!" className={styles.nav_font}>Question</a></div>
-            </div>
-            <img className={styles.mainpagebeforeloginlogo} src="/imgs/nav_logo.png" />
-        </div>
+        <NavigationLeft>
+            <SecondButton>Sign Up</SecondButton>
+            <FirstButton>Login</FirstButton>
+            <Links2>
+              <LinkDiv><NavFont href="#!">Home</NavFont></LinkDiv>
+              <LinkDiv><NavFont href="#!">Question</NavFont></LinkDiv>
+            </Links2>
+            <LogoImage src="/imgs/nav_logo.png" />
+        </NavigationLeft>
     );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,116 @@
-import React from 'react';
-import { useState } from 'react';
-import styles from './NavBar.module.css'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const NavigationLeft = styled.div`
+    background-color: #ffffff;
+    height: 64px;
+    left: 318px;
+    position: absolute;
+    top: 0;
+    width: 1440px;
+`;
+
+const FirstButton = styled.button`
+    color: #000000;
+    font-family: "Roboto", Helvetica;
+    font-size: 18px;
+    font-weight: 400;
+    height: 40px;
+    right: 11%;
+    position: absolute;
+    top: 12px;
+    width: 10%;
+    background-color: #ffffff;
+`;
+
+const SecondButton = styled.button`
+    height: 40px;
+    right: 0.7%;
+    position: absolute;
+    top: 12px;
+    width: 10%;
+    background-color: rgb(0,0,0);
+    color: rgb(255,255,255);
+`;
+
+const ThirdButton = styled.button`
+    color: #0044bb;
+    font-family: "Roboto", Helvetica;
+    font-size: 18px;
+    font-weight: 400;
+    height: 40px;
+    right: 11%;
+    position: absolute;
+    top: 12px;
+    width: 10%;
+    background-color: #ffffff;
+`;
+
+const FourthButton = styled.button`
+    color: #0044bb;
+    font-family: "Roboto", Helvetica;
+    font-size: 18px;
+    font-weight: 400;
+    height: 40px;
+    right: 21%;
+    position: absolute;
+    top: 12px;
+    width: 10%;
+    background-color: #ffffff;
+`;
+
+const LinkDiv = styled.div`
+    color: #000000;
+    font-family: "Roboto", Helvetica;
+    font-size: 18px;
+    font-weight: 400;
+    height: 22px;
+    left: 0;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 0;
+`;
+
+const Links2 = styled.div`
+    height: 22px;
+    left: 156px;
+    position: absolute;
+    top: 21px;
+    width: 165px;
+`;
+
+const NavFont = styled.a`
+    font-family: "Roboto", Helvetica; font-size: 18px; font-weight: 400; color: rgb(0, 0, 0); line-height: normal;
+    height: 22px;
+    left: 0;
+    letter-spacing: 0;
+    position: absolute;
+    top: 0;
+`;
+
+const TextWrapper = styled.div`
+    color: #000000;
+    font-family: "Roboto", Helvetica;
+    font-size: 18px;
+    font-weight: 400;
+    height: 22px;
+    left: 80px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 0;
+`;
+
+const LogoImage = styled.img`
+    height: 58px;
+    left: 20px;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    width: 101px;
+    cursor: pointer;
+`;
 
 function NavBar(props) {
     const [isDetailClicked, setDetailClicked] = useState(false);
@@ -10,23 +120,23 @@ function NavBar(props) {
     }
 
     return (
-        <div className={styles.navigationleft}>
+        <NavigationLeft>
             {!isDetailClicked ? <>
-            <button className={styles.secondBtn} onClick={detailHandler}>{props.userName}님</button>
-            <button className={styles.firstBtn}>고객센터</button>
+            <SecondButton onClick={detailHandler}>{props.userName}님</SecondButton>
+            <FirstButton>고객센터</FirstButton>
             </> :
             <>
-            <button className={styles.secondBtn} onClick={detailHandler}>Back</button>
-            <button className={styles.thirdBtn}>Log Out</button>
-            <button className={styles.fourthBtn}>My Page</button>
+            <SecondButton onClick={detailHandler}>Back</SecondButton>
+            <ThirdButton>Log Out</ThirdButton>
+            <FourthButton>My Page</FourthButton>
             </>
             }
-            <div className={styles.links2}>
-              <div className={styles.link}><a href="#!" className={styles.nav_font}>Home</a></div>
-              <div className={styles.textwrapper}><a href="#!" className={styles.nav_font}>Question</a></div>
-            </div>
-            <img className={styles.mainpagebeforeloginlogo} src="/imgs/nav_logo.png" />
-        </div>
+            <Links2>
+              <LinkDiv><NavFont href="#!">Home</NavFont></LinkDiv>
+              <TextWrapper><NavFont href="#!">Question</NavFont></TextWrapper>
+            </Links2>
+            <LogoImage src="/imgs/nav_logo.png" />
+        </NavigationLeft>
     );
 }
 
