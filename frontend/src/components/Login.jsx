3ce660f3504994b91f80c42caaf0863927564b66
SeoJeongYeop <sjyswe99@gmail.com>
2023-05-31 22:00:25 +0900

    feat(frontend): local storage에  access_token 저장

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,95 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const LoginContainer = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoContainer = styled.div`
+  width: 100%;
+  height: 60px;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoImg = styled.img`
+  width: 5%;
+  aspect-ratio: 9/5;
+  margin-top: 10px;
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #f2f2f2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoginTitle = styled.h1`
+  margin: 0;
+  padding-top: 100px;
+  font-family: Roboto;
+  color: #000000;
+  font-size: 64px;
+  font-weight: 500;
+`;
+
+const LoginButton = styled.button`
+  margin-top: 20px;
+  width: 20%;
+  aspect-ratio: 4/1;
+  font-family: Roboto;
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+const GithubLoginButton = styled(LoginButton)`
+  background-color: #555555;
+`;
+
+const CustomerSupportButton = styled(LoginButton)`
+  background-color: #959595;
+`;
+
+const FooterText = styled.p`
+  margin: 0;
+  padding-top: 35vh;
+  font-family: Roboto;
+  color: #000000;
+  font-size: 14px;
+  font-weight: 100;
+`;
 
 const Login = () => {
+  const handleGithubLogin = () => {
+    // local 테스트용
+    window.location.href = 'https://github.com/login/oauth/authorize?client_id=Iv1.f8333c935b2b0479';
+  };
+
   return (
-    <div style={{width: "100vw", flexDirection:"column", textAlign: "center"}}>
-      <div style={{width: "100vw", height: "60px", padding: "0", margin: "0", top: "0"}}><img style={{width: "5%", aspectRatio: "9/5", top: "10px"}} src="/imgs/nav_logo.png" /></div>
-      <div style={{width: "100vw", height: "100vh", backgroundColor: "#f2f2f2", top: "0", margin: "0px"}}>
-        <p style={{margin: "0", paddingTop: "100px", fontFamily: "Roboto", color: "#000000", fontSize: "64px", fontWeight: "500"}}>로그인</p>
-        <button style={{marginTop: "110px", width: "20%", aspectRatio: "4/1", fontFamily: "Roboto", color: "#ffffff", backgroundColor: "#555555", fontSize: "18px", fontWeight: "500"}}>Github로 로그인</button>
-        <br />
-        <button style={{marginTop: "10px", width: "20%", aspectRatio: "8/1", fontFamily: "Roboto", color: "#ffffff", backgroundColor: "#959595", fontSize: "18px", fontWeight: "500"}}>고객 센터</button>
-        <br />
-        <p style={{margin: "0", paddingTop: "35vh", fontFamily: "Roboto", color: "#000000", fontSize: "14px", fontWeight: "100"}}>© 2023 -    Privacy — Terms</p>
-      </div>
-    </div>
+    <LoginContainer>
+      <LogoContainer>
+        <LogoImg src="/imgs/nav_logo.png" alt="Logo" />
+      </LogoContainer>
+      <ContentContainer>
+        <LoginTitle>로그인</LoginTitle>
+        <GithubLoginButton onClick={handleGithubLogin}>Github로 로그인</GithubLoginButton>
+        <CustomerSupportButton>고객 센터</CustomerSupportButton>
+        <FooterText>© 2023 - Privacy — Terms</FooterText>
+      </ContentContainer>
+    </LoginContainer>
   );
 };
 
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,9 +1,10 @@
-export { default as Main } from './MainPage'
-export { default as Produce } from './ProducePage'
-export { default as Home } from './HomePage'
-export { default as Auth } from './AuthPage'
-export { default as Profile } from './ProfilePage'
-export { default as Problem } from './ProblemPage'
-export { default as Notice} from './NoticePage'
-export { default as Questions} from './QuestionsPage'
-export { default as Review } from './ReviewPage'
+export { default as Main } from './MainPage';
+export { default as Produce } from './ProducePage';
+export { default as Home } from './HomePage';
+export { default as Auth } from './AuthPage';
+export { default as Profile } from './ProfilePage';
+export { default as Problem } from './ProblemPage';
+export { default as Notice } from './NoticePage';
+export { default as Questions } from './QuestionsPage';
+export { default as Review } from './ReviewPage';
+export { default as OAuth } from './OAuthPage';
