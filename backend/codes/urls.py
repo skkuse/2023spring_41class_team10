214f47d1840072f5ca0bc5657df97130d2dbd58b
SeongBoKyung <qhrud3500@gmail.com>
2023-06-01 22:19:10 +0900

    feat: Review, Comment, Deadcode 기능 추가

--- backend/codes/urls.py
+++ backend/codes/urls.py
@@ -5,4 +5,7 @@ app_name = 'codes'
 
 urlpatterns = [
     path('v1/refactoring/', ChatRefactorAPIView.as_view(), name='refactor_api'),
+    path('v1/review/', ChatReviewAPIView.as_view(), name='review_api'),
+    path('v1/comment/', ChatAddCommentAPIView(), name='add_comment_api'),
+    path('v1/deadcode/', ChatDeadcodeAPIView(), name='deadcode_api'),
 ]
